5eea530fcc59df60f02ea579cead72759d0e7041
Satira <90534931+ArtemMazyev210@users.noreply.github.com>
2021-10-24 22:58:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: false,
+        slidesToShow: 4, // Вот тут изменять кол-во показанных слайдов
+        slidesToScroll: 4,
+        speed: 1000, // Скорость скрола
+        easing: 'ease', // Настроить анимацию слайдов
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true, // auto playing
+        autoplaySpeed: 10000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: true, // swipe for pc
+        touchThreshold: 10,
+        touchMove: true,
+        waitForAnimate: false,
+        centerMode: false,
+        responsive: [{ // adaptive
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false,
+                waitForAnimate: true
+            }
+        }],
+    });
+});
